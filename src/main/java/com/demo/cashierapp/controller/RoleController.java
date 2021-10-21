8339a88f62491a72efde84969ae81a2b02bade36
Ruzanna Zohrabyan <ruzanna83@gmail.com>
2021-10-22 02:13:26 +0400

    Made all required changes. Working version

--- src/main/java/com/demo/cashierapp/controller/RoleController.java
+++ src/main/java/com/demo/cashierapp/controller/RoleController.java
@@ -3,12 +3,14 @@ package com.demo.cashierapp.controller;
 import com.demo.cashierapp.api.service.role.RoleApiService;
 import com.demo.cashierapp.entity.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/api/role")
 @RequiredArgsConstructor
